91723c54c020df7850d851bf2eb23f4a27365455
thierry-rhone <65720627+thierry-rhone@users.noreply.github.com>
2022-05-03 17:35:35 +0200

    Update media_player.py

--- custom_components/sfrtv/media_player.py
+++ custom_components/sfrtv/media_player.py
@@ -6,11 +6,12 @@ import logging
 import socket
 import json
 import voluptuous as vol
-
+import aiofiles as aiof
+import time
 from . import exceptions
-
+from datetime import timedelta
 from . remote import Remote
-
+from homeassistant.util import dt as dt_util
 try:
     from homeassistant.components.media_player import MediaPlayerEntity, PLATFORM_SCHEMA
 except ImportError:
@@ -36,17 +37,10 @@ from homeassistant.const import (
     STATE_OFF,
     STATE_ON,
     STATE_UNKNOWN,
-    CONF_PORT,
-
+    STATE_PAUSED,
+    STATE_PLAYING,
     )
 import homeassistant.helpers.config_validation as cv
-
-
-from homeassistant.util.json import load_json
-
-
-
-
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -55,16 +49,12 @@ CONF_TIMEOUT = 'timeout'
 DEFAULT_NAME = 'SFR TV Remote'
 DEFAULT_PORT = 7682
 DEFAULT_TIMEOUT = 20
-DEFAULT_SOURCE_LIST = '{"TF1": "1", "France 2": "2","France 3": "3"}'
-CONF_SOURCE_LIST = "source_list"
-
 
 KNOWN_DEVICES_KEY = 'STB7_known_devices'
 
 ATTR_MEDIA_DESCRIPTION = "media_description"
 ATTR_MEDIA_END_TIME = "media_end_time"
 ATTR_MEDIA_START_TIME = "media_start_time"
-
 
 SUPPORT_SFRTV = (
     SUPPORT_PAUSE
@@ -84,46 +74,32 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
     vol.Required(CONF_HOST): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
     vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
-    vol.Optional(CONF_SOURCE_LIST, default=DEFAULT_SOURCE_LIST): cv.string,
-
     }
 )
 
 # pylint: disable=unused-argument
-def setup_platform(hass, config, add_devices, discovery_info=None):
+async def async_setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the SFR TV platform."""
     known_devices = hass.data.get(KNOWN_DEVICES_KEY)
     if known_devices is None:
         known_devices = set()
         hass.data[KNOWN_DEVICES_KEY] = known_devices
 
-
-    # Is this a manual configuration?
     if config.get(CONF_HOST) is not None:
         host = config.get(CONF_HOST)
-        port = config.get(CONF_PORT)
         name = config.get(CONF_NAME)
-        source_list = config.get(CONF_SOURCE_LIST)
         timeout = config.get(CONF_TIMEOUT)
-    elif discovery_info is not None:
-        tv_name = discovery_info.get('name')
-        model = discovery_info.get('model_name')
-        host = discovery_info.get('host')
-        name = "{} ({})".format(tv_name, model)
-        port = DEFAULT_PORT
-        timeout = DEFAULT_TIMEOUT
-        source_list = DEFAULT_SOURCE_LIST
     else:
         _LOGGER.warning("Cannot determine device")
         return
 
+    port = DEFAULT_PORT
     config_path = hass.config.path("custom_components/sfrtv/sources.json")
-    with open(config_path, encoding="utf8") as sourcesfile:
-        source_list = sourcesfile.read()
+    async with aiof.open(config_path, encoding="utf8") as sourcesfile:
+        source_list = await sourcesfile.read()
 
-    # Only add a device once, so discovered devices do not override manual
+    # Only add a device once
     # config.
     ip_addr = socket.gethostbyname(host)
     if ip_addr not in known_devices:
@@ -133,13 +109,11 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     else:
         _LOGGER.info("Ignoring duplicate SFR TV %s:%d", host, port)
 
-
 class SfrTVDevice(MediaPlayerEntity):
     """Representation of a SFR TV."""
 
     def __init__(self, host, port, name, timeout, source_list):
         """Initialize the SFR device."""
-
         # Save a reference to the imported classes
         self._exceptions_class = exceptions
         self._remote_class = Remote
@@ -148,10 +122,9 @@ class SfrTVDevice(MediaPlayerEntity):
         # Assume that the TV is not muted
         self._muted = False
         # Assume that the TV is in Play mode
-        self._playing = True
         self._state = STATE_UNKNOWN
         self._remote = None
-        # Mark the end of a shutdown command (need to wait 15 seconds before
+        # Mark the end of a shutdown command (need to wait 5 seconds before
         # sending the next command to avoid turning the TV back ON).
         self._end_of_power_off = None
         self._source = None
@@ -170,7 +143,7 @@ class SfrTVDevice(MediaPlayerEntity):
 
     def update(self):
         """Retrieve the latest data."""
-        # Send an empty key to see if we are still connected
+        # get session status
         self.send_key('GETINFO', '', '')
 
     def get_remote(self):
@@ -178,32 +151,16 @@ class SfrTVDevice(MediaPlayerEntity):
         if self._remote is None:
             # We need to create a new instance to reconnect.
             self._remote = self._remote_class(self._config)
-
         return self._remote
 
     def send_key(self, key, keyarg1, keyarg2):
         """Send a key to the tv and handles exceptions."""
         _LOGGER.debug("Sending command: %s %s %s", key, keyarg1, keyarg2)
-        if self._power_off_in_progress() and not (key == 'POWER'):
-            _LOGGER.info("TV is powering off, not sending command: %s", key)
+        if self._power_off_in_progress() and not (key in ('POWER', 'GETINFO')):
+            _LOGGER.info("TV is powering off, not sending command: %s  ", key)
             return
         try:
-            resp_dict = self.get_remote().control(key, keyarg1, keyarg2 )
-            if resp_dict.get ('Action') == "GetSessionsStatus":
-                if resp_dict.get ('RemoteResponseCode') == "OK":
-                    self._state = STATE_ON
-                    if resp_dict['Data']['LiveSession']['Speed'] == 1 :
-                        self._playing = True
-                    else:
-                        self._playing = False
-                    self._source = resp_dict['Data']['LiveSession']['LiveItem']['CurrentChannel']['Name']
-                else:
-                    self._state = STATE_OFF
-
-
-
-
-
+          self.get_remote().control(key, keyarg1, keyarg2 )
         except (self._exceptions_class.UnhandledResponse,
                 self._exceptions_class.AccessDenied, BrokenPipeError):
             # We got a response so it's on.
@@ -217,10 +174,21 @@ class SfrTVDevice(MediaPlayerEntity):
         if self._power_off_in_progress():
             self._state = STATE_OFF
 
+        time.sleep(0.2)
+        resp_dict = self.get_remote().read_response ()
+        if resp_dict.get ('Action') == "GetSessionsStatus":
+            if resp_dict.get ('RemoteResponseCode') == "OK":
+                self._source = resp_dict['Data']['LiveSession']['LiveItem']['CurrentChannel']['Name']
+                if resp_dict['Data']['LiveSession']['Speed'] == 1 :
+                    self._state = STATE_PLAYING
+                else:
+                    self._state = STATE_PAUSED
+            else:
+                self._state = STATE_OFF
 
     def _power_off_in_progress(self):
-        return self._end_of_power_off is not None
-
+        return self._end_of_power_off is not None and \
+               self._end_of_power_off > dt_util.utcnow()
 
     @property
     def name(self):
@@ -236,7 +204,6 @@ class SfrTVDevice(MediaPlayerEntity):
     def source(self):
         """Return the current input source."""
         return self._source
-
 
     @property
     def is_volume_muted(self):
@@ -263,7 +230,6 @@ class SfrTVDevice(MediaPlayerEntity):
     @property
     def extra_state_attributes(self):
         """Return device specific state attributes.
-
         isRecording:        Is the box currently recording.
         currservice_fulldescription: Full program description.
         currservice_begin:  is in the format '21:00'.
@@ -279,14 +245,15 @@ class SfrTVDevice(MediaPlayerEntity):
     def turn_off(self):
         """Turn off media player."""
         _LOGGER.debug("turning off stb7")
-    #    self._end_of_power_off = dt_util.utcnow() + timedelta(seconds=15)
+        self._end_of_power_off = dt_util.utcnow() + timedelta(seconds=4)
         self.send_key('BUTTONEVENT', 'POWER', '')
 
     def turn_on(self):
         """Turn the media player on."""
-        _LOGGER.debug("turning off stb7")
+        _LOGGER.debug("turning on stb7")
         self.send_key('BUTTONEVENT', 'POWER', '')
-
+        time.sleep(2)
+        self.send_key('BUTTONEVENT', 'OK', '')
 
     def volume_up(self):
         """Volume up the media player."""
@@ -305,27 +272,17 @@ class SfrTVDevice(MediaPlayerEntity):
         """Send mute command."""
         self.send_key('BUTTONEVENT', 'MUTE', '')
 
-    def media_play_pause(self):
-        """Simulate play pause media player."""
-        if self._playing:
-            self.media_pause()
-        else:
-            self.media_play()
-
     def media_play(self):
         """Send play command."""
-        if self._playing is False:
-            self.send_key('BUTTONEVENT', 'PLAYPAUSE', '')
+        self.send_key('BUTTONEVENT', 'PLAYPAUSE', '')
 
     def media_pause(self):
         """Send media pause command to media player."""
-        if self._playing is True:
-            self.send_key('BUTTONEVENT', 'PLAYPAUSE', '')
+        self.send_key('BUTTONEVENT', 'PLAYPAUSE', '')
 
     def media_stop(self):
         """Send media pause command to media player."""
         self.send_key('BUTTONEVENT', 'STOP', '')
-
 
     def media_next_track(self):
         """Send next track command."""
@@ -335,15 +292,8 @@ class SfrTVDevice(MediaPlayerEntity):
         """Send the previous track command."""
         self.send_key('BUTTONEVENT', 'REWIND', '')
 
-
     def select_source(self, source):
         """Select input source."""
         self._current_source = source
         if source in self._source_list:
             self.send_key('ZAP', self._source_list[source], '')
-
-
-
-
-
-
